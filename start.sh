605f5b93ed5df31ab15a090c82e379b48520c52e
Melvin <31448155+melvio@users.noreply.github.com>
2021-04-05 12:39:22 +0200

    connect properly

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ cd || exit 1
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo apt-get install -y python3-dev nginx python3-venv
+sudo apt-get install -y python3-dev nginx python3-venv uwsgi-emperor
 
 cd ~/webapp/backend || exit 1
 python3 -m venv venv
@@ -19,4 +19,7 @@ read -ri "did you make a .env file?"
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
 
-sudo cp ~/webapp/etc/ /etc
+cd ~/webapp/etc/ || exit 1
+sudo cp -r ./* /etc/
+
+sudo systemctl enable nginx
