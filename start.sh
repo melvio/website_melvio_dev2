dec6dc9f1dda36fcda75125e0190deebdae92ee2
Melvin <31448155+melvio@users.noreply.github.com>
2021-04-05 11:37:47 +0200

    update to config

--- start.sh
+++ start.sh
@@ -6,9 +6,13 @@ cd || exit 1
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install nginx python3-venv
+sudo apt-get install python3-dev nginx python3-venv
 
 cd ~/webapp/backend || exit 1
 python3 -m venv venv
 source venv/bin/activate
+python3 pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8001
